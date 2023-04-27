72926d5495a38e9bec8d4284373af000add5ef4c
naman shah <93568656+namanshah22@users.noreply.github.com>
2023-04-27 21:09:20 +0530

    Add files via upload

---
+++ FirstFit.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+class FirstFit
+{
+public static void main(String args[])
+{
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter number of memory partitions: ");
+int totalMem = sc.nextInt();
+int part[] = new int[totalMem];
+int i, j;
+for(i = 0; i < totalMem; i++)
+{
+System.out.print("Enter memory in memory partition "+(i+1)+": ");
+part[i] = sc.nextInt();
+}
+System.out.print("\nEnter number of process to be added to main memory: ");
+int n = sc.nextInt();
+int mem_p[] = new int[n];
+int flag[] = new int[part.length];
+for(i = 0; i < n; i++)
+{
+System.out.print("Enter memory to be assigned to process "+(i+1)+": ");
+mem_p[i] = sc.nextInt();
+}
+for(i = 0; i < part.length; i++)
+flag[i] = 0;
+int id;
+for(i = 0; i < n; i++)
+{
+id = -1;
+for(j = 0; j < part.length; j++)
+{
+if((flag[j] == 0) && (mem_p[i] <= part[j]))
+{
+id = j;
+break;
+}
+}
+if(id != -1)
+{
+System.out.println("Process "+(i+1)+"\tMemory Allocated\tPartition: "+part[id]);
+flag[id] = 1;
+}
+else
+System.out.println("Process "+(i+1)+"\tMemory Not Allocated");
+}
+}
+}
