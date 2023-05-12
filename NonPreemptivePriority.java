ba70a0c23928de83d12149333bb6cca4e2770523
naman shah <93568656+namanshah22@users.noreply.github.com>
2023-05-12 18:39:20 +0530

    Add files via upload

---
+++ NonPreemptivePriority.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+class NonPreemptivePriority {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.print("Enter number of processes to be executed: ");
+        n = sc.nextInt();
+        int p[] = new int[n];
+        int at[] = new int[n];
+        int bt[] = new int[n];
+        int pt[] = new int[n]; // priority
+        int i;
+        for(i = 0; i < n; i++) {
+            p[i] = i+1;
+            System.out.print("Enter arrival time: ");
+            at[i] = sc.nextInt();
+            System.out.print("Enter burst time: ");
+            bt[i] = sc.nextInt();
+            System.out.print("Enter priority: ");
+            pt[i] = sc.nextInt();
+        }
+        int ct[] = new int[n];
+        int cur_t = 0;
+        int st[] = new int[n];
+        boolean done[] = new boolean[n];
+        int min_pt, min_idx;
+        int remaining = n;
+        while(remaining > 0) {
+            min_pt = Integer.MAX_VALUE;
+            min_idx = -1;
+            for(i = 0; i < n; i++) {
+                if(!done[i] && at[i] <= cur_t && pt[i] < min_pt) {
+                    min_pt = pt[i];
+                    min_idx = i;
+                }
+            }
+            if(min_idx == -1) {
+                cur_t++;
+            } else {
+                ct[min_idx] = cur_t + bt[min_idx];
+                done[min_idx] = true;
+                remaining--;
+                st[min_idx] = cur_t;
+                cur_t = ct[min_idx];
+            }
+        }
+        int tat[] = new int[n];
+        int wt[] = new int[n];
+        float sum1 = 0, sum2 = 0;
+        System.out.println("\nProcess No.\tA.T\tB.T.\tC.T.\tT.A.T.\tW.T.");
+        for(i = 0; i < n; i++) {
+            tat[i] = ct[i] - at[i];
+            wt[i] = tat[i] - bt[i];
+            System.out.println("Process "+p[i]+"\t"+at[i]+"\t"+bt[i]+"\t"+ct[i]+"\t"+tat[i]+"\t"+wt[i]);
+            sum1 += tat[i];
+            sum2 += wt[i];
+        }
+        System.out.println("Average Turn Around Time: "+(sum1/n));
+        System.out.println("Average Waiting Time: "+(sum2/n));
+        System.out.println("\n\t\tGANTT CHART\nPROCESS \tStart Time\tCompletion Time");
+        for(i = 0; i < n; i++) {
+            System.out.println("PROCESS "+p[i]+"\t\t"+st[i]+"\t\t"+ct[i]);
+        }
+    }
+}
