d36613d9c9a7d89e39577c7dbb73837af0088630
naman shah <93568656+namanshah22@users.noreply.github.com>
2023-05-13 23:59:10 +0530

    better

--- Producer-consumer.c
+++ Producer-consumer.c
@@ -1,39 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
-int mutex = 1;
-int products = 0;
-int emptyspace = 5, x = 0;
-int Wait(int n)
+int semaphore = 1;
+int full = 0;
+int empty = 5, x = 0;
+#define MAX_BUFFER_SIZE 100
+#define buffer_size 8
+int buffer[buffer_size];    // shared buffer
+int in = 0;     // index for next item to be produced
+int out = 0;    // index for next item to be consumed
+//int buffer_size; 
+int temp;  
+int item;      // size of buffer
+int down(int n)
 {
  --n;
  return n;
 }
-int Signal(int n)
+int up(int n)
 {
  ++n;
  return n;
 }
+void produce_item(int temp) {
+    printf("enter item");
+    scanf("%d",&temp);
+    printf("produced item %d\n",temp);  
+}
+
+void consume_item(int item) {
+    printf("Consumed item %d\n", item);
+}
 void producer()
 {
- mutex = Wait(mutex);
- products = Signal(products);
- emptyspace = Wait(emptyspace);
- x = Signal(x);
- printf("Producer produces item : %d\n",x);
- mutex = Signal(mutex);
+ produce_item(temp);
+ semaphore = down(semaphore);
+ empty = down(empty);
+ buffer[in]=temp;
+ in=(in+1) % buffer_size;
+ semaphore=up(semaphore);
+ full=up(full);
 }
 void consumer()
 {
- mutex = Wait(mutex);
- products = Wait(products);
- emptyspace = Signal(emptyspace);
- printf("Consumer consumes item : %d\n",x);
- x = Wait(x);
- mutex = Signal(mutex);
+ //produce_item(temp);
+ full = down(full);
+ semaphore = down(semaphore);
+ item=buffer[out];
+ //buffer[in]=temp;
+ out=(out+1) % buffer_size;
+ semaphore=up(semaphore);
+ empty=up(down);
+ consume_item(item);
 }
 int main()
 {
  int n, i=1;
+ 
  printf("\n1. Press 1 for Producer\n2. Press 2 for Consumer\n3. Press 3 for Exit");
  while(i>0)
  {
@@ -42,13 +64,13 @@ int main()
  switch (n)
  {
  case 1:
- if ((mutex == 1) && (emptyspace != 0))
+ if ((semaphore == 1) && (empty !=0))
  producer();
  else
  printf("Buffer is Full\nProducer cannot produce more items\n");
  break;
  case 2:
- if ((mutex == 1) && (products != 0))
+ if ((semaphore == 1) && (full != 0))
  consumer();
  else
  printf("Buffer is empty\nConsumer not allowed to consume\n");
