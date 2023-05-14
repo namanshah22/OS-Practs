31c6f731d1b37ef72055b19ff2457f5a879a1a7b
naman shah <93568656+namanshah22@users.noreply.github.com>
2023-05-14 15:04:42 +0530

    Add files via upload

---
+++ LFU.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class LFU {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int num, n, i;
+        System.out.print("Enter no of frames in main memory: ");
+        num = sc.nextInt();
+        int frame[] = new int[num];
+        int count[] = new int[num];
+        for (i = 0; i < num; i++) {
+            frame[i] = -1;
+            count[i] = 0;
+        }
+        System.out.print("Enter no of pages to be executed: ");
+        n = sc.nextInt();
+        int page_stream[] = new int[n];
+        System.out.println("Enter page stream:");
+        for (i = 0; i < n; i++)
+            page_stream[i] = sc.nextInt();
+        int count1 = 0, count2 = 0;
+        System.out.println("\nPage Stream");
+        for (i = 0; i < n; i++) {
+            boolean flag = false;
+            for (int j = 0; j < num; j++) {
+                if (frame[j] == page_stream[i]) {
+                    flag = true;
+                    count[j]++;
+                    count2++;
+                    break;
+                }
+            }
+            if (!flag) {
+                int lfuIndex = 0;
+                for (int j = 1; j < num; j++) {
+                    if (count[j] < count[lfuIndex]) {
+                        lfuIndex = j;
+                    }
+                }
+                frame[lfuIndex] = page_stream[i];
+                count[lfuIndex] = 1;
+                count1++;
+            }
+            System.out.print(page_stream[i] + "\t\t\t");
+            for (int j = 0; j < num; j++) {
+                if (frame[j] != -1)
+                    System.out.print(frame[j] + "\t");
+                else
+                    System.out.print("\t");
+            }
+            System.out.println(flag ? "H" : "F");
+        }
+        float hit_ratio, miss_ratio;
+        hit_ratio = (float) (count2) / (float) (n);
+        miss_ratio = (float) (count1) / (float) (n);
+        System.out.println("\n\nNo. of page hits = " + count2);
+        System.out.println("No. of page faults = " + count1);
+        System.out.println("Hit ratio = " + hit_ratio);
+        System.out.println("Miss ratio = " + miss_ratio);
+    }
+}
+ 
+    
+
